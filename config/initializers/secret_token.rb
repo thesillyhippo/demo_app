5a103aa09c0126b8e2674fe58de39db9d2f21295
obviate <hungryhippo138@gmail.com>
2011-07-26 17:22:28 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c213ab365c6aa1527285772bfebb19da30d3102ab42fd22debf9bed96e70e97bf2befa88d2dcbc8349d8aa20aaf04c7802d1b047cc3bce31a7b8d4eca7945390'
